5c10cf37a685bb0db463dd2c04c6e2a87aa04caf
luxiansheng <luxiansheng@zto.cn>
2019-07-11 20:27:35 +0800

    style: format only

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parserOptions: {
     // https://eslint.vuejs.org/user-guide/#usage
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
     ecmaVersion: 2019
   },
   env: {
@@ -10,22 +10,22 @@ module.exports = {
   },
   extends: [
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    "standard",
+    'standard',
     // rules for vue, didn't include any js rules lib like standard or airbnb
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention.
-    "plugin:vue/recommended",
+    'plugin:vue/recommended',
     // turn off ESLint default stylistic rules
     // https://github.com/prettier/eslint-config-prettier/blob/master/index.js
-    "prettier",
+    'prettier',
     // turn off stylistic rules defined in standard
     // https://github.com/prettier/eslint-config-prettier/blob/master/standard.js
-    "prettier/standard",
+    'prettier/standard',
     // turn off stylistic rules defined in plugin:vue/recommended
     // https://github.com/prettier/eslint-config-prettier/blob/master/vue.js
-    "prettier/vue"
+    'prettier/vue'
   ],
   // required to lint index.html and *.vue files
-  plugins: ["vue"],
+  plugins: ['vue'],
   // check if imports actually resolve
   // settings: {
   //   'import/resolver': {
@@ -42,66 +42,66 @@ module.exports = {
   rules: {
     //! avoid add stylistic rules
     // non stylistic rules which are not in the standard but we might need
-    "no-debugger": "error",
-    "no-console": "error",
-    "no-extra-semi": "error",
-    "no-prototype-builtins": "error",
-    "require-atomic-updates": "error",
-    "array-callback-return": ["error", { allowImplicit: true }],
-    "block-scoped-var": "error",
-    "guard-for-in": "error",
-    "no-alert": "error",
-    "no-implicit-coercion": "error",
-    "no-extra-label": "error",
-    "no-eq-null": "error",
-    "no-else-return": "error",
-    "no-case-declarations": "error",
-    "vars-on-top": "error",
+    'no-debugger': 'error',
+    'no-console': 'error',
+    'no-extra-semi': 'error',
+    'no-prototype-builtins': 'error',
+    'require-atomic-updates': 'error',
+    'array-callback-return': ['error', { allowImplicit: true }],
+    'block-scoped-var': 'error',
+    'guard-for-in': 'error',
+    'no-alert': 'error',
+    'no-implicit-coercion': 'error',
+    'no-extra-label': 'error',
+    'no-eq-null': 'error',
+    'no-else-return': 'error',
+    'no-case-declarations': 'error',
+    'vars-on-top': 'error',
     // "require-unicode-regexp": "error",
-    "require-await": "error",
-    radix: "error",
-    "prefer-promise-reject-errors": ["error", { allowEmptyReject: true }],
-    "no-void": "error",
-    "no-useless-concat": "error",
-    "no-useless-catch": "error",
-    "no-script-url": "error",
-    "no-return-await": "error",
-    "no-undef": "error",
-    "no-use-before-define": ["error", { functions: false, classes: true }],
-    "no-delete-var": "error",
-    "no-label-var": "error",
-    "no-restricted-globals": [
-      "error",
+    'require-await': 'error',
+    radix: 'error',
+    'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }],
+    'no-void': 'error',
+    'no-useless-concat': 'error',
+    'no-useless-catch': 'error',
+    'no-script-url': 'error',
+    'no-return-await': 'error',
+    'no-undef': 'error',
+    'no-use-before-define': ['error', { functions: false, classes: true }],
+    'no-delete-var': 'error',
+    'no-label-var': 'error',
+    'no-restricted-globals': [
+      'error',
       {
-        name: "event",
-        message: "Use local parameter instead."
+        name: 'event',
+        message: 'Use local parameter instead.'
       }
     ],
     // todo not good enough for confusing let foo=()=>{}
-    "arrow-body-style": ["error", "as-needed"],
-    "arrow-parens": ["error", "as-needed"],
-    "no-confusing-arrow": ["error", { allowParens: true }],
-    "no-var": "error",
-    "prefer-const": "error",
-    "prefer-rest-params": "error",
-    "prefer-spread": "error",
-    "require-yield": "error",
-    "symbol-description": "error",
+    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-confusing-arrow': ['error', { allowParens: true }],
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'require-yield': 'error',
+    'symbol-description': 'error',
     // eslint-plugin-vue
-    "vue/v-on-function-call": ["error", "never"],
-    "vue/require-direct-export": "error",
+    'vue/v-on-function-call': ['error', 'never'],
+    'vue/require-direct-export': 'error',
     // 'vue/no-empty-pattern': 'error',
-    "vue/eqeqeq": "error",
-    "vue/comma-dangle": "error",
+    'vue/eqeqeq': 'error',
+    'vue/comma-dangle': 'error',
     // known bug, so turned off https://github.com/vuejs/eslint-plugin-vue/issues/862
     // https://github.com/vuejs/eslint-plugin-vue/issues/864
     // you should disable vue/prop-name-casing, vue/attribute-hyphenation
     // when handling history files and enable it in daily development
-    "vue/prop-name-casing": "off",
+    'vue/prop-name-casing': 'off',
     // https://github.com/vuejs/eslint-plugin-vue/issues/924
-    "vue/attribute-hyphenation": "off",
+    'vue/attribute-hyphenation': 'off',
     // too many history errors
-    "vue/name-property-casing": "off"
+    'vue/name-property-casing': 'off'
     // too many violations
     // 'vue/component-name-in-template-casing': [
     //   'error',
@@ -112,4 +112,4 @@ module.exports = {
     //   }
     // ]
   }
-};
+}
